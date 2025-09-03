3f3bbb9592ac115a8c4c1b5032f992327d2e5a45
Michał Stankiewicz <kontakt@stankiewiczm.eu>
2025-09-03 13:22:26 +0200

    Update CACHE_VERSION to 'ztkweb-v11' (#24)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'ztkweb-v10';
+const CACHE_VERSION = 'ztkweb-v11';
 const STATIC_CACHE = CACHE_VERSION + '-static';
 const DATA_CACHE = CACHE_VERSION + '-data';
 const REMOTE_CACHE = CACHE_VERSION + '-remote';
